df59c27bf8f654894095a9fd7d9c4151d61517f9
Michal Front <michal.front@outlook.com>
2021-08-25 22:19:31 +0200

    Tryial and error

--- src/App.js
+++ src/App.js
@@ -1,31 +1,70 @@
 import './App.css';
-import {arr} from './components/data'
+import {useState} from 'react';
+import {cards} from './components/data'
 import Button from './components/Button'
 
 function App() {
+  const [current, setCurrent] = useState(0);
+  const length = cards.length;
+  const [isFirst, setIsFirst] = useState(null);
+
+  const prevCard = () => {
+        setCurrent(current === 0 ? 0 : current - 1 )
+  }
+
+  const nextCard = () => {
+    setCurrent(current === length -1? length-1 : current + 1  )
+  }
+
+  //const printCurrent = (e) => {
+  //  console.log('e.currentTarget: ', e.currentTarget.id);
+  //  let clicked = e.currentTarget.id;
+  //  console.log('clicked === current: ', clicked === current);
+  //}
+
+    //if should be continuous
+  //const prevCard = () => {
+  //      setCurrent(current === 0 ? length-1 : current - 1 )
+  //}
+
+  //const nextCard = () => {
+  //  setCurrent(current === length -1? 0 : current + 1  )
+  //}
+
+  //console.log('current: ', current);
+
   return (
     <div className="App">
-      <h1 className="head">Latana Knowledge Center</h1>
-      <h6>Everything you need to know about the best brand tracking solution in the world.</h6>
+      <header className="head">
+        <h1>Latana Knowledge Center</h1>
+        <p>Everything you need to know about the best brand tracking solution in the world.</p>
+      </header>
       <div className="carousel-container">
-        {arr && (
-          arr.map((item, i) => {
+        {cards && (
+          cards.map((item) => {
             return (
-              <div key={i} className="carousel-card">
+              <div 
+              key={item.id} 
+              className="carousel-card" 
+              id={item.id} 
+              //onClick={printCurrent}
+              >
                 <img src={item.src} alt="" />
-                <h2>{item.header}</h2>
-                <p>{item.a}</p>
-                <p>{item.b}</p>
-                <p>{item.c}</p>
-                <Button />
+                <div className="card-text">
+                  <h2>{item.header}</h2>
+                  <p>{item.a}</p>
+                  <p>{item.b}</p>
+                  <p>{item.c}</p>
+                </div>
+                <Button to={item.href}/>
               </div>
             )
           })
         )}
-
+      <div className="opaque"></div>
       </div>
-
-
+      <button className="prev" onClick={prevCard}>Prev</button>
+      <button className="next" onClick={nextCard}>Next</button>
     </div>
   );
 }
--- src/components/data.js
+++ src/components/data.js
@@ -1,11 +1,53 @@
-export const arr = [
+//import dataimg from '/assets/images/data.png'
+import whatis from './assets/images/whatis.png'
+import started from './assets/images/started.png'
+import using from './assets/images/using.png'
+import dataimg from './assets/images/data.png'
+
+export const cards = [
     {
         id: 0,
         header: "What is Latana?",
         a: "How does Latana work?",
         b: "Why choose Latana?",
         c: "What does Latana cost?",
-        src: "./assets/images/whatis.png",
+        src: whatis,
         href: "",
-    }
+    },
+    {
+        id: 1,
+        header: "Getting Started",
+        a: "What is MRP?",
+        b: "How do you gather data?",
+        c: "What is a margin of error?",
+        src: started,
+        href: "",
+    },
+    {
+        id: 2,
+        header: "Using Latana",
+        a: "How to use Latana?",
+        b: "What is the Brand Funnel?",
+        c: "How to interpret Brand Tracking Data?",
+        src: using,
+        href: "",
+    },
+    {
+        id: 3,
+        header: "Data Collection & Methodology",
+        a: "What is MRP?",
+        b: "How do you gather data?",
+        c: "What is a margin of error?",
+        src: dataimg,
+        href: "",
+    },
+    {
+        id: 4,
+        header: "Success",
+        a: "What is MRP?",
+        b: "How do you gather data?",
+        c: "What is a margin of error?",
+        src: using,
+        href: "",
+    },
 ]
